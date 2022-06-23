d4735b0dffe41f9c4a717b9c62e6737c347680ef
petermonky <wjdgustjr0554@gmail.com>
2022-06-23 23:12:22 +0800

    add number row for keys

--- src/data/keys.ts
+++ src/data/keys.ts
@@ -1,4 +1,5 @@
 export const KEYS = {
+  numRow: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
   topRow: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   middleRow: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   bottomRow: ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
