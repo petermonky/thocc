0c5b5473b6f10c1eadc94114d84f9f9460c88802
petermonky <wjdgustjr0554@gmail.com>
2022-06-23 23:12:37 +0800

    fix minor styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./components/app";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './components/app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
