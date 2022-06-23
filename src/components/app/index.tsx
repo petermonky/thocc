a24f5d37573336ea93af7c8538a9117a2385c79f
petermonky <wjdgustjr0554@gmail.com>
2022-06-23 23:13:02 +0800

    prevent all default key down events

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,13 +1,27 @@
+import { useEffect } from 'react';
 import './App.scss';
 
 import Key from '../key';
 import { KEYS } from '../../data/keys';
 
 function App() {
-  const { topRow, middleRow, bottomRow } = KEYS;
+  const { numRow, topRow, middleRow, bottomRow } = KEYS;
+
+  useEffect(() => {
+    const keyDownListener = (event: KeyboardEvent) => event.preventDefault();
+
+    document.addEventListener('keydown', keyDownListener);
+
+    return () => document.removeEventListener('keydown', keyDownListener);
+  });
 
   return (
     <div className="container">
+      <div>
+        {numRow.map((key) => (
+          <Key key={key} letter={key} />
+        ))}
+      </div>
       <div>
         {topRow.map((key) => (
           <Key key={key} letter={key} />
